7052136a8f5340bf191ee3663ae5a09edbc307b5
Gabe Rogan <gabe@futo.org>
2024-01-23 14:46:14 -0500

    add test function

---
+++ functions/uploader.ts
@@ -0,0 +1,23 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = 'https://postman-echo.com/post'
+
+    const headers = {
+      'content-type': 'application/json;charset=UTF-8',
+    }
+
+    const response1 = await fetch(url, {
+      headers,
+      method: 'POST',
+      body: '{ "foo": 2 }',
+    })
+
+    const response2Text = await fetch(url, {
+      headers,
+      method: 'POST',
+      body: response1.body,
+    }).then((res) => res.text())
+
+    return new Response(response2Text, { headers })
+  },
+}
