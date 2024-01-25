13c06d27255a821634c89653c696e9c395e27908
Gabe Rogan <gabe@futo.org>
2024-01-25 11:30:13 -0500

    cloudflare plugin pages

--- functions/youtube-login-redirect.ts
+++ functions/youtube-login-redirect.ts
@@ -1,5 +1,5 @@
 import type { EventContext, Response as TResponse } from '@cloudflare/workers-types'
 
 export async function onRequest(ctx: EventContext<any, any, any>): Promise<TResponse> {
-  return new Response('hello world') as any
+  return new Response('hello world yt') as any
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite'
+import cloudflarePages from './vite-plugin-cloudflare-pages'
+
+const PORT = 3000
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), cloudflarePages()],
+  server: {
+    port: PORT,
+    hmr: {
+      port: PORT,
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
 })
