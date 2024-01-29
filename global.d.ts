9ab3acd143e4d31002c6b585494424524d77614f
Gabe Rogan <gabe@futo.org>
2024-01-29 15:24:50 -0500

    remove JWT

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,6 @@ interface Env {
   DB: import('@cloudflare/workers-types').D1Database
   YOUTUBE_CLIENT_SECRET_JSON: string
   ORIGIN: string
-  JWT_SECRET: string
   [key: string]: string | null
 }
 
